10e034c831f38fac2e8fbb07455c8183fbfa75e4
Junxiao Shi <git@mail1.yoursunny.com>
2022-02-21 21:58:48 -0500

    ci,etc: clang-format-8 => clang-format-11

--- mk/format-code.sh
+++ mk/format-code.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
 find -name '*.[hc]pp' -or -name '*.ino' | \
-  xargs clang-format-8 -i -style=file
+  xargs clang-format-11 -i -style=file
